73d90f9fc3b861f4fdc073f064a96185a93150e2
00L1nce <Lance.Yapuchura@fundacion-jala.org>
2022-10-05 17:33:33 -0400

    TAREA_1_DINO
    Se agacha el dinosaurio :D :100:

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -20,6 +20,7 @@ class Dinosaur(Sprite):
         self.step_index = 0
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
         self.jump_vel = self.JUMP_VEL
 
     def update(self, user_input):
@@ -27,13 +28,21 @@ class Dinosaur(Sprite):
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
         
-        if user_input[pygame.K_UP] and not self.dino_jump:
+        if user_input[pygame.K_UP] and not self.dino_jump and not user_input[pygame.K_DOWN]:
             self.dino_jump = True
             self.dino_run = False
+            self.dino_duck = False
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_jump = False
+            self.dino_run = False
+            self.dino_duck = True
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
+            self.dino_duck = False
 
         if self.step_index >= 10:
             self.step_index = 0
@@ -52,7 +61,12 @@ class Dinosaur(Sprite):
             self.dino_rect = False
             self.jump_vel = self.JUMP_VEL
 
-#tarea. hacer que se agache
+    def duck(self):
+        self.image = DUCKING[0] if  self.step_index < 5  else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = (self.Y_POS + 40)
+        self.step_index += 1
 
 
     def run(self):
